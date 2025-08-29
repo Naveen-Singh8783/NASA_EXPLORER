97bb88a5ed6edbe87c55c2815e1c604880f1803e
Naveen Singh <naveensingh4237@gmail.com>
2025-08-29 13:04:26 +0100

    added mar photos api

--- backEnd/server.js
+++ backEnd/server.js
@@ -37,6 +37,27 @@ app.get('/api/apod', async(req, res) => {
 });
 
 
+    //Mors rover photos 
+    app.get('/api/mars', async(req, res) => {
+       try{
+
+        const { earth_date = "2015-06-03", rover = "curiosity", camera } = req.query;
+        const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos`;
+        const { data } = await axios.get(url, {
+             params: {api_key: NASA_KEY, date};
+        });
+        res.json(data);
+        }catch(err){
+            console.error(err?.response?.data || err.message);
+            res.status(err?.response?.status || 500).json({
+            error: "Failed to fetch Mars photos",
+            details: err?.response?.data || err.message
+             });
+        }
+    });
+
+
+
 app.listen(PORT, () => {
      console.log(`Backend running on http://localhost:${PORT}`);
 })
